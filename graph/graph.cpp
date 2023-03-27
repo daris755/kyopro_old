2952b61033447e0e6b44973a2a22f3368566b1bc
daris755 <71542143+daris755@users.noreply.github.com>
2023-03-27 10:25:35 +0900

    Create graph.cpp

---
+++ graph/graph.cpp
@@ -0,0 +1,12 @@
+// グラフ
+vector<vector<int>> graph(int n, int m, bool directed = 0, int origin = 1) {
+	vector<vector<int>> g(n);
+	while (m--) {
+		int u, v;
+		cin >> u >> v;
+		u -= origin, v -= origin;
+		g[u].emplace_back(v);
+		if (!directed) g[v].emplace_back(u);
+	}
+	return g;
+}
