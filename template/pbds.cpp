e3c3e087763d3aa79a02b96f1f7577323450de57
daris755 <71542143+daris755@users.noreply.github.com>
2023-03-30 15:27:06 +0900

    Update pbds.cpp

--- template/pbds.cpp
+++ template/pbds.cpp
@@ -2,3 +2,4 @@
 #include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/tag_and_trait.hpp>
 using namespace __gnu_pbds;
+template<class T, class Ta, class Tb> using Tree = tree<T, Ta, Tb, rb_tree_tag, tree_order_statistics_node_update>;
