fd80e900955954f18b355a38a12a88fe42712157
daris755 <71542143+daris755@users.noreply.github.com>
2023-03-31 09:45:44 +0900

    Create isSquare.cpp

---
+++ geometry/isSquare.cpp
@@ -0,0 +1,13 @@
+// 正方形判定
+bool isSquare(vector<int>& x, vector<int>& y) {
+	vector<int> v;
+	for (int i = 0; i < 4; i++) {
+		for (int j = i + 1; j < 4; j++) {
+			v.push_back((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
+		}
+	}
+	sort(all(v));
+	int l = v[0];
+	if (l == 0) return 0; // 座標の重複判定
+	return v[0] == l && v[1] == l && v[2] == l && v[3] == l && v[4] == l + l && v[5] == l + l;
+}
