4b4284bfbb33d9c3a6822f35e156b10b1d6fc710
daris755 <71542143+daris755@users.noreply.github.com>
2023-03-31 10:28:11 +0900

    Create enumDvisors.cpp

---
+++ prime/enumDvisors.cpp
@@ -0,0 +1,11 @@
+vector<ll> enumDivisors(ll n) {
+	vector<ll> res;
+	for (ll i = 1; i * i <= n; i++) {
+		if (n % i == 0) {
+			res.push_back(i);
+			if (n / i != i) res.push_back(n / i);
+		}
+	}
+	sort(all(res));
+	return res;
+}
