ce10640abd56a01df771a547135528dc69b088c3
daris755 <71542143+daris755@users.noreply.github.com>
2023-03-31 09:52:39 +0900

    Update unfinished.cpp

--- unfinished.cpp
+++ unfinished.cpp
@@ -51,66 +51,6 @@ vector<bool> Eratosthenes(size_t n) {
 
 
 
-//正方形判定
-bool isSquare(vector<int>& x, vector<int>& y) {
-	vector<int> v;
-	for (int i = 0; i < 4; i++) {
-		for (int j = i + 1; j < 4; j++) {
-			v.push_back((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
-		}
-	}
-	sort(all(v));
-	int l = v[0];
-	if (l == 0) return 0; //座標の重複判定
-	return v[0] == l && v[1] == l && v[2] == l && v[3] == l && v[4] == l + l && v[5] == l + l;
-}
-
-struct UnionFind {
-	vector<int> par, siz;
-
-	// 初期化
-	UnionFind(int n) : par(n, -1), siz(n, 1) { }
-
-	// 根を求める(経路圧縮あり)
-	int root(int x) {
-		if (par[x] == -1) return x; // ｘが根の場合はｘを返す
-		return par[x] = root(par[x]);
-	}
-
-	// ｘとｙが同じグループに属するかどうか（根が一致するかどうか）
-	bool issame(int x, int y) {
-		return root(x) == root(y);
-	}
-
-	// ｘを含むグループとｙを含むグループを併合する
-	bool unite(int x, int y) {
-		// ｘ，ｙをそれぞれ根まで移動する
-		x = root(x);
-		y = root(y);
-
-		// すでに同じグループのときは何もしない
-		if (x == y) return false;
-
-		// union by size（ｙ側のサイズが小さくなるようにする）
-		if (siz[x] < siz[y]) swap(x, y);
-
-		//ｙをｘの子とする
-		par[y] = x;
-		siz[x] += siz[y];
-		return true;
-	}
-
-	// ｘを含むグループのサイズ
-	int size(int x) {
-		return siz[root(x)];
-	}
-};
-
-void charm() {
-	cin.tie(nullptr);
-	ios::sync_with_stdio(false);
-	cout << fixed << setprecision(15);
-}
 
 
 // 行列乗算
