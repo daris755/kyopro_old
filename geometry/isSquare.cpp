8676cbd6b1aa8b6b959e36e325a3a350274e73e5
daris755 <71542143+daris755@users.noreply.github.com>
2023-04-01 12:30:35 +0900

    Update isSquare.cpp

--- geometry/isSquare.cpp
+++ geometry/isSquare.cpp
@@ -6,7 +6,7 @@ bool isSquare(vector<int>& x, vector<int>& y) {
 			v.push_back((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
 		}
 	}
-	sort(all(v));
+	sort(v.begin(), v.end());
 	int l = v[0];
 	if (l == 0) return 0; // 座標の重複判定
 	return v[0] == l && v[1] == l && v[2] == l && v[3] == l && v[4] == l + l && v[5] == l + l;
